fdaf4bd1d506617a4735a9c9e8b5bc6a4605b648
hkobori-cq <h.kobori@caraquri.com>
2016-09-08 01:13:32 +0900

    API通信の形完成

--- app/src/main/java/com/caraquri/bookmanager_android/activity/MainActivity.java
+++ app/src/main/java/com/caraquri/bookmanager_android/activity/MainActivity.java
@@ -8,26 +8,20 @@ import android.util.Log;
 
 import com.caraquri.bookmanager_android.R;
 import com.caraquri.bookmanager_android.adapter.BookTitleAdapter;
-import com.caraquri.bookmanager_android.api.BookDataClient;
+import com.caraquri.bookmanager_android.api.APIListener;
+import com.caraquri.bookmanager_android.api.ConnectAPI;
 import com.caraquri.bookmanager_android.databinding.ActivityMainBinding;
 import com.caraquri.bookmanager_android.model.BookDataModel;
-import com.google.gson.Gson;
-import com.squareup.okhttp.OkHttpClient;
 
 
 import java.util.ArrayList;
 import java.util.List;
 
-import retrofit.GsonConverterFactory;
-import retrofit.Retrofit;
-import retrofit.RxJavaCallAdapterFactory;
-import rx.Observer;
-import rx.Subscription;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements APIListener {
     public ActivityMainBinding mainBinding;
-    protected Subscription subscription;
+    private MainActivity mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,30 +31,14 @@ public class MainActivity extends Activity {
     }
 
     private void initRecyclerView(){
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://app.com")
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .build();
-        BookDataClient.BookDataService service = retrofit.create(BookDataClient.BookDataService.class);
-
-        this.subscription = service.getBookData("0-10")
-                .subscribe(new Observer<String>() {
-                    @Override
-                    public void onCompleted() {
-                        Log.d("","onComplete");
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.e("", "エラー : " + e.toString());
-                    }
-
-                    @Override
-                    public void onNext(String s) {
-                        Log.d("","onNext");
-                    }
-                });
+        (new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ConnectAPI connectAPI = new ConnectAPI("page=0-10","getBook");
+                connectAPI.setAPIListener(mainActivity);
+                connectAPI.execute();
+            }
+        })).start();
         List<BookDataModel> list = new ArrayList<>();
         list.add(new BookDataModel("item1"));
         list.add(new BookDataModel("item2"));
@@ -71,6 +49,11 @@ public class MainActivity extends Activity {
         BookTitleAdapter adapter = new BookTitleAdapter(list);
         mainBinding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
         mainBinding.recyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public void succeededAPIConnection(StringBuffer result){
+        Log.d("ok","ok");
     }
 
 }
---
+++ app/src/main/java/com/caraquri/bookmanager_android/api/APIListener.java
@@ -0,0 +1,5 @@
+package com.caraquri.bookmanager_android.api;
+
+public interface APIListener {
+    void succeededAPIConnection(StringBuffer result);
+}
