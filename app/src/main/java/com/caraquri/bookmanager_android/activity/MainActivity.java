5e0c0342544f9d6237b07b199c6a95acfdda641f
hkobori-cq <h.kobori@caraquri.com>
2016-09-07 14:17:54 +0900

    とりあえずretrofitが使えるように設定

--- app/src/main/java/com/caraquri/bookmanager_android/activity/MainActivity.java
+++ app/src/main/java/com/caraquri/bookmanager_android/activity/MainActivity.java
@@ -4,19 +4,28 @@ import android.app.Activity;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.util.Log;
 
 import com.caraquri.bookmanager_android.R;
 import com.caraquri.bookmanager_android.adapter.BookTitleAdapter;
+import com.caraquri.bookmanager_android.api.BookDataClient;
 import com.caraquri.bookmanager_android.databinding.ActivityMainBinding;
 import com.caraquri.bookmanager_android.model.BookDataModel;
+import com.squareup.okhttp.OkHttpClient;
+
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends Activity {
-    public BookTitleAdapter bookTitleAdapter;
-    public ActivityMainBinding mainBinding;
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.GsonConverterFactory;
+import retrofit.Response;
+import retrofit.Retrofit;
 
+
+public class MainActivity extends Activity {
+    public ActivityMainBinding mainBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +35,36 @@ public class MainActivity extends Activity {
     }
 
     private void initRecyclerView(){
+        OkHttpClient httpClient = new OkHttpClient();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://app.com")
+                .client(httpClient)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        BookDataClient.BookDataService service = retrofit.create(BookDataClient.BookDataService.class);
+
+        Call<String> call = service.getBookData("0-10");
+        call.enqueue(new Callback<String>() {
+            @Override
+            public void onResponse(Response<String> response, Retrofit retrofit) {
+                Log.v("", Boolean.toString(response.isSuccess()));
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Log.e("error",t.toString());
+            }
+        });
         List<BookDataModel> list = new ArrayList<>();
         list.add(new BookDataModel("item1"));
         list.add(new BookDataModel("item2"));
-
+        list.add(new BookDataModel("item3"));
+        list.add(new BookDataModel("item4"));
+        list.add(new BookDataModel("item5"));
+        list.add(new BookDataModel("item6"));
         BookTitleAdapter adapter = new BookTitleAdapter(list);
         mainBinding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
         mainBinding.recyclerView.setAdapter(adapter);
     }
+
 }
--- app/src/main/java/com/caraquri/bookmanager_android/model/BookDataModel.java
+++ app/src/main/java/com/caraquri/bookmanager_android/model/BookDataModel.java
@@ -1,9 +1,14 @@
 package com.caraquri.bookmanager_android.model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 public class BookDataModel {
-    public final String bookName;
     public BookDataModel(String bookName){
         this.bookName = bookName;
     }
+
+    @Expose
+    @SerializedName("book_name")
+    public String bookName;
 }
