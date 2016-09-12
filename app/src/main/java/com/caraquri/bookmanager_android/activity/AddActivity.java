75bd78f91b87b04fa893f2809c267d00a46a973e
hkobori-cq <h.kobori@caraquri.com>
2016-09-12 11:57:49 +0900

    AddActivityにFragmentを導入

--- app/src/main/java/com/caraquri/bookmanager_android/activity/AddActivity.java
+++ app/src/main/java/com/caraquri/bookmanager_android/activity/AddActivity.java
@@ -1,6 +1,17 @@
 package com.caraquri.bookmanager_android.activity;
 
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.caraquri.bookmanager_android.R;
+import com.caraquri.bookmanager_android.databinding.ActivityAddBinding;
+
 public class AddActivity extends AppCompatActivity {
+    protected ActivityAddBinding binding;
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_add);
+    }
 }
---
+++ app/src/main/java/com/caraquri/bookmanager_android/fragment/BookRegisterFragment.java
@@ -0,0 +1,24 @@
+package com.caraquri.bookmanager_android.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.caraquri.bookmanager_android.R;
+import com.caraquri.bookmanager_android.databinding.FragmentAddViewBinding;
+
+public class BookRegisterFragment extends Fragment{
+    protected FragmentAddViewBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return inflater.inflate(R.layout.fragment_add_view,container,false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        binding = FragmentAddViewBinding.bind(getView());
+    }
+}
