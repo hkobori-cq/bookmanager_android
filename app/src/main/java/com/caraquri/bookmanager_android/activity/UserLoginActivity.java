39106fb5f9fe2dd2d9935a45b62d76291d3ed79b
hkobori-cq <h.kobori@caraquri.com>
2016-09-15 16:29:39 +0900

    LoginページののActivity作成

---
+++ app/src/main/java/com/caraquri/bookmanager_android/activity/UserLoginActivity.java
@@ -0,0 +1,31 @@
+package com.caraquri.bookmanager_android.activity;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import com.caraquri.bookmanager_android.R;
+import com.caraquri.bookmanager_android.databinding.ActivityUserLoginBinding;
+
+public class UserLoginActivity extends AppCompatActivity {
+    protected ActivityUserLoginBinding binding;
+    private static final String TAG = UserLoginActivity.class.getSimpleName();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_user_login);
+        initToolbar();
+    }
+
+    private void initToolbar(){
+        setSupportActionBar(binding.toolbar);
+        ActionBar bar = getSupportActionBar();
+        if (bar != null) {
+            bar.setDisplayHomeAsUpEnabled(true);
+            bar.setDisplayShowHomeEnabled(true);
+        }
+    }
+}
