d2ab6422f82c3ad2e363cae6db73a197f1f1d8de
hkobori-cq <h.kobori@caraquri.com>
2016-09-26 14:28:15 +0900

    UserLoginActivityのToolbar処理をUserLoginFragmentに移植

--- app/src/main/java/com/caraquri/bookmanager_android/activity/UserLoginActivity.java
+++ app/src/main/java/com/caraquri/bookmanager_android/activity/UserLoginActivity.java
@@ -1,24 +1,11 @@
 package com.caraquri.bookmanager_android.activity;
 
-import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.widget.EditText;
 
 import com.caraquri.bookmanager_android.R;
-import com.caraquri.bookmanager_android.api.DataClient;
 import com.caraquri.bookmanager_android.databinding.ActivityUserLoginBinding;
-import com.caraquri.bookmanager_android.fragment.AlertDialogFragment;
-
-import retrofit.Call;
-import retrofit.Callback;
-import retrofit.Response;
-import retrofit.Retrofit;
 
 public class UserLoginActivity extends AppCompatActivity {
     private static final String TAG = UserLoginActivity.class.getSimpleName();
@@ -33,75 +20,5 @@ public class UserLoginActivity extends AppCompatActivity {
 
     private void initToolbar() {
         setSupportActionBar(binding.toolbar);
-        ActionBar bar = getSupportActionBar();
-        if (bar != null) {
-            bar.setDisplayHomeAsUpEnabled(true);
-            bar.setDisplayShowHomeEnabled(true);
-        }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_user_settings, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_user_settings:
-                registerUserData();
-                break;
-        }
-        return true;
-    }
-
-    /**
-     * 保存ボタンを押したときに呼ばれるメソッド
-     */
-    public void registerUserData() {
-        EditText email = (EditText) findViewById(R.id.user_login_email_field);
-        EditText password = (EditText) findViewById(R.id.user_login_password_field);
-
-        String emailStr = email.getText().toString();
-        String passwordStr = password.getText().toString();
-
-        Bundle args = new Bundle();
-        AlertDialogFragment alertDialog = new AlertDialogFragment();
-
-        if (emailStr.isEmpty()) {
-            args.putString(AlertDialogFragment.ALERT_DIALOG_MESSAGE_KEY, getString(R.string.input_mail_address));
-            alertDialog.setArguments(args);
-            alertDialog.show(getSupportFragmentManager(), AlertDialogFragment.ALERT_DIALOG_SHOW_KEY);
-        } else if (passwordStr.isEmpty()) {
-            args.putString(AlertDialogFragment.ALERT_DIALOG_MESSAGE_KEY, getString(R.string.input_password));
-            alertDialog.setArguments(args);
-            alertDialog.show(getSupportFragmentManager(), AlertDialogFragment.ALERT_DIALOG_SHOW_KEY);
-        } else {
-            DataClient client = new DataClient();
-            Call<Integer> call = client.userLoginClient(emailStr, passwordStr);
-            call.enqueue(new Callback<Integer>() {
-                @Override
-                public void onResponse(Response<Integer> response, Retrofit retrofit) {
-                    if (response.isSuccess()) {
-                        Intent intent = new Intent(UserLoginActivity.this, MainActivity.class);
-                        startActivity(intent);
-                    } else {
-                        Bundle args = new Bundle();
-                        AlertDialogFragment alertDialog = new AlertDialogFragment();
-                        args.putString(AlertDialogFragment.ALERT_DIALOG_MESSAGE_KEY, getString(R.string.failed_login_message));
-                        alertDialog.setArguments(args);
-                        alertDialog.show(getSupportFragmentManager(), AlertDialogFragment.ALERT_DIALOG_SHOW_KEY);
-                    }
-                }
-
-                @Override
-                public void onFailure(Throwable t) {
-
-                }
-            });
-        }
     }
 }
--- app/src/main/java/com/caraquri/bookmanager_android/fragment/UserLoginFragment.java
+++ app/src/main/java/com/caraquri/bookmanager_android/fragment/UserLoginFragment.java
@@ -1,21 +1,33 @@
 package com.caraquri.bookmanager_android.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.caraquri.bookmanager_android.R;
+import com.caraquri.bookmanager_android.activity.MainActivity;
+import com.caraquri.bookmanager_android.api.DataClient;
 import com.caraquri.bookmanager_android.databinding.FragmentUserLoginBinding;
 import com.caraquri.bookmanager_android.util.KeyboardUtil;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
 
 public class UserLoginFragment extends Fragment {
     protected FragmentUserLoginBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
         return inflater.inflate(R.layout.fragment_user_login, container, false);
     }
 
@@ -24,6 +36,22 @@ public class UserLoginFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         binding = FragmentUserLoginBinding.bind(getView());
         onTextFieldUnFocused();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_user_settings, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_user_settings:
+                registerUserData();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -40,6 +68,47 @@ public class UserLoginFragment extends Fragment {
                 return true;
             }
         });
+    }
+
+    private void registerUserData() {
+        String emailStr = binding.userLoginEmailField.getText().toString();
+        String passwordStr = binding.userLoginPasswordField.getText().toString();
+
+        Bundle args = new Bundle();
+        AlertDialogFragment alertDialog = new AlertDialogFragment();
+
+        if (emailStr.isEmpty()) {
+            args.putString(AlertDialogFragment.ALERT_DIALOG_MESSAGE_KEY, getString(R.string.input_mail_address));
+            alertDialog.setArguments(args);
+            alertDialog.show(getActivity().getSupportFragmentManager(), AlertDialogFragment.ALERT_DIALOG_SHOW_KEY);
+        } else if (passwordStr.isEmpty()) {
+            args.putString(AlertDialogFragment.ALERT_DIALOG_MESSAGE_KEY, getString(R.string.input_password));
+            alertDialog.setArguments(args);
+            alertDialog.show(getActivity().getSupportFragmentManager(), AlertDialogFragment.ALERT_DIALOG_SHOW_KEY);
+        } else {
+            DataClient client = new DataClient();
+            Call<Integer> call = client.userLoginClient(emailStr, passwordStr);
+            call.enqueue(new Callback<Integer>() {
+                @Override
+                public void onResponse(Response<Integer> response, Retrofit retrofit) {
+                    if (response.isSuccess()) {
+                        Intent intent = new Intent(getActivity(), MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        Bundle args = new Bundle();
+                        AlertDialogFragment alertDialog = new AlertDialogFragment();
+                        args.putString(AlertDialogFragment.ALERT_DIALOG_MESSAGE_KEY, getString(R.string.failed_login_message));
+                        alertDialog.setArguments(args);
+                        alertDialog.show(getActivity().getSupportFragmentManager(), AlertDialogFragment.ALERT_DIALOG_SHOW_KEY);
+                    }
+                }
+
+                @Override
+                public void onFailure(Throwable t) {
+
+                }
+            });
+        }
     }
 
 
